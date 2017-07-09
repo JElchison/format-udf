df5332feefa0eb28698dfcb8f9ff6cce8a2d1aad
Jonathan Elchison <JElchison@Gmail.com>
2017-07-09 15:37:57 -0400

    Adapt unit-test.sh for older version of losetup

--- unit-test.sh
+++ unit-test.sh
@@ -25,7 +25,7 @@ sync
 # create loop device
 sudo losetup -f $IMAGE_FILE
 # get path to new loop device
-DEVICE=$(losetup | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
+DEVICE=$(losetup -a | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
 
 
 # perform the UDF format.
@@ -43,7 +43,7 @@ DEVICE=
 # create loop device
 sudo losetup -f $IMAGE_FILE
 # get path to new loop device
-DEVICE=$(losetup | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
+DEVICE=$(losetup -a | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
 
 # mount image
 udisksctl mount -b "/dev/$DEVICE"
@@ -72,7 +72,7 @@ LOCATION=
 # create loop device
 sudo losetup -f $IMAGE_FILE
 # get path to new loop device
-DEVICE=$(losetup | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
+DEVICE=$(losetup -a | grep $IMAGE_FILE | head -n 1 | awk '{print $1}' | sed -r 's|^/dev/||')
 
 # mount image
 udisksctl mount -b /dev/loop0
