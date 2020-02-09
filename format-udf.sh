ec5fd9a2a743f4d37b1415f925f0e711ae8f8d5d
Jonathan Elchison <JElchison@Gmail.com>
2020-02-09 07:33:38 -0500

    Update version to 1.7.1

--- format-udf.sh
+++ format-udf.sh
@@ -47,7 +47,7 @@ fi
 ###############################################################################
 
 # version of this script
-VERSION=1.6.1
+VERSION=1.7.1
 # maximum number of heads per cylinder
 HPC=255
 # maximum number of sectors per track
