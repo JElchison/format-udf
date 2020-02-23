9d670210a3f7cf2992a521765af6137d40c4e8b5
Jonathan Elchison <JElchison@Gmail.com>
2020-02-23 14:41:18 -0500

    Update manual-test-macos.sh

--- manual-test-macos.sh
+++ manual-test-macos.sh
@@ -8,11 +8,11 @@ DEVICE=$1
 
 ./format-udf.sh -v
 ./format-udf.sh -h
-./format-udf.sh -b 512 -f "/dev/$DEVICE" "UDF Manual Test 1"
+./format-udf.sh -f "/dev/$DEVICE" "UDF Manual Test 1"
 ./format-udf.sh -b 512 -f "$DEVICE" "UDF Manual Test 2"
 ./format-udf.sh -b 512 -f -p mbr "$DEVICE" "UDF Manual Test 3"
 ./format-udf.sh -b 512 -f -p none "$DEVICE" "UDF Manual Test 4"
 ./format-udf.sh -b 512 -f -w quick "$DEVICE" "UDF Manual Test 5"
 ./format-udf.sh -b 512 -f -w zero "$DEVICE" "UDF Manual Test 6"
-./format-udf.sh -b 512 -f -w scrub "$DEVICE" "UDF Manual Test 7"
+#./format-udf.sh -b 512 -f -w scrub "$DEVICE" "UDF Manual Test 7"
 
